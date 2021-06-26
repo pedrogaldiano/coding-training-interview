b21aed64ca7e04d54f030dd4466ac246bcac935e
Pedro Galdiano <pedrocastro014@gmail.com>
2021-06-26 17:32:07 -0300

    update

---  merge-sorted-array.py
+++  merge-sorted-array.py
@@ -1,11 +1,9 @@
-nums1 = [1,2,3,0,0,0]
-nums2 = [2,5,6]
-m = 3
-
-nums1 = nums1[:m]
-nums1 += nums2
-nums1 = sorted(nums1)
-
-print(nums1)
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1[m:] = nums2
+        nums1.sort()
 
 
--- testing.py
+++ testing.py
@@ -1,5 +1,11 @@
-while True:
-    for i in range(10):
-        print(i)
-    else:
-        print('break')
+import random
+
+nums1 = [random.choice(range(1,10000)) for i in range(50)] + [0] * 50
+
+nums2 = [random.choice(range(1,10000)) for i in range(50)]
+
+print(nums1)
+print(len(nums1))
+print()
+print(nums2)
+print(len(nums2))
